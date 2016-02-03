282a54c5f98d7d1407c56c2a44b7096ae5bd5fb8
DarkAce65 <tvdood99@gmail.com>
2016-02-02 21:51:18 -0500

    Add game rules object

--- quantumChess.js
+++ quantumChess.js
@@ -1,11 +1,12 @@
-var board;
+var board, game;
 
 $(document).ready(function() {
 	var config = {
 		"draggable": true,
-		"notation": false,
+		"showNotation": false,
 		"pieceTheme": "lib/chessboardjs/img/chesspieces/wikipedia/{piece}.png",
 		"position": "start"
 	};
 	board = ChessBoard("board", config); // Initialize chessboard
+	game = new Chess(); // Game rules
 });
