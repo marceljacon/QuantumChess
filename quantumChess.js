f6dfb51b8bad2b9a13224c343f0482990a4836d7
DarkAce65 <tvdood99@gmail.com>
2016-02-02 21:43:09 -0500

    Create board config json

--- quantumChess.js
+++ quantumChess.js
@@ -1,3 +1,11 @@
+var board;
+
 $(document).ready(function() {
-	var board = ChessBoard("board");
+	var config = {
+		"draggable": true,
+		"notation": false,
+		"pieceTheme": "chessboardjs/img/chesspieces/wikipedia/{piece}.png",
+		"position": "start"
+	};
+	board = ChessBoard("board", config); // Initialize chessboard
 });
