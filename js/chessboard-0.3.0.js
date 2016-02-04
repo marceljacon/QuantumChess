1b33febbcaf5723ccc5aa77de84492a12e6717e0
DarkAce65 <tvdood99@gmail.com>
2016-02-04 10:35:33 -0500

    Start working on config

--- js/chessboard-0.3.0.js
+++ js/chessboard-0.3.0.js
@@ -241,6 +241,8 @@ var ANIMATION_HAPPENING = false,
 	BOARD_BORDER_SIZE = 2,
 	CURRENT_ORIENTATION = 'white',
 	CURRENT_POSITION = {},
+	CURRENT_PRIMARY = {},
+	CURRENT_SECONDARY = {},
 	SQUARE_SIZE,
 	DRAGGED_PIECE,
 	DRAGGED_PIECE_LOCATION,
@@ -467,8 +469,19 @@ function expandConfig() {
 		cfg.trashSpeed = 100;
 	}
 
+	//
+	if (cfg.hasOwnProperty('quantum') === true && cfg.quantum === true) {
+		if (cfg.hasOwnProperty('secondaryPosition') === true && validPositionObject(cfg.position) === true) {
+			CURRENT_POSITION = deepCopy(START_POSITION);
+			CURRENT_PRIMARY = deepCopy(START_POSITION);
+		}
+
+		else {
+			error(7263, 'Invalid value passed to config.secondaryPosition.', cfg.secondaryPosition);
+		}
+	}
 	// make sure position is valid
-	if (cfg.hasOwnProperty('position') === true) {
+	else if (cfg.hasOwnProperty('position') === true) {
 		if (cfg.position === 'start') {
 			CURRENT_POSITION = deepCopy(START_POSITION);
 		}
